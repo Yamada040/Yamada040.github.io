b8c57f9d10cf6015afb011c767e9b2645da5dd62
秋人 <syuuto20030912@icloud.com>
2025-01-14 16:01:49 +0900

    Fixed font and link issue

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,5 @@ const nextConfig: NextConfig = {
   /* config options here */
 
 };
-// next.config.js
-module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true, // ビルド中にESLintチェックを無効化
-  },
-};
-
 
 export default nextConfig;
